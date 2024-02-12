6a343bd8189fee8841cdad9eea16b814eade547f
BestEngineer <65943291+bestpisit@users.noreply.github.com>
2024-02-12 22:52:31 +0700

    basic matrix

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import numpy as np
 
-x = np.array([[ 1, 10, 1, 3, 5],
-            [ 1, 15, 2, 3, 5],
-            [ 0, 20, 3, 3, 5],
-            [ 1, 25, 4, 3, 5]])
+x = np.array([[1,2],[3,4]])
+y = np.array([[1,1],[1,1]])
+print(x)
+print(y)
 
-y = (x[:,0] == 1)
-
-print(np.sum(x[y][:,[2,4]],axis=1))
+print(x*y); print()
+print(x@y)
