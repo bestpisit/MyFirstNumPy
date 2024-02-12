8b3adca2881ef4f0e59ce06ad60aed010ffd3e3a
BestEngineer <65943291+bestpisit@users.noreply.github.com>
2024-02-12 22:37:49 +0700

    array slice index

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import numpy as np
 import math
 
-x = np.array([[1,5,3,1],[0,1,2,9]])
+# assign values
+x = np.array([
+    [ 5, 6, 7, 8],
+	  [ 9,10,11,12],
+	  [13,14,15,16]])
 
 print(x); print()
-print(f"np.sum(x): {np.sum(x)}")                    # sum of all axes
-print(f"np.sum(x, axis=0): {np.sum(x, axis=0)}")    # column sum
-print(f"np.sum(x, axis=1): {np.sum(x, axis=1)}")    # row sum
+x[:,2:] = 0
+print(x)
