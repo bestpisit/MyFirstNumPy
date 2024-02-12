54f58b9c350618a0923aea173e9664ad92e799f8
BestEngineer <65943291+bestpisit@users.noreply.github.com>
2024-02-12 21:57:23 +0700

    numpy array contruction and type, shape

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
 import numpy as np
 print(np.__version__)
+
+x1 = np.array([1,2,3,4,5,6])                      # 1D array
+x2 = np.array([[1,2,3],[11,22,33],[10,20,30]])    # 2D array
+
+print('---Print numpy arrays:---')
+print(f"x1 1D-array: \n{x1}")
+print(f"x2 2D-array: \n{x2}")
+
+print('\n------Print 2D list------')
+y =[[1,2,3],[11,22,33],[10,20,30]]
+print(f"y 2D-List: \n{y}")
+
+print(x1.shape,x1.dtype)
+print(x2.shape,x2.dtype)
