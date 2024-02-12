59339410d08ccdc1a44b91c0a398b963f94b1fc4
BestEngineer <65943291+bestpisit@users.noreply.github.com>
2024-02-12 22:28:45 +0700

    statistic

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import numpy as np
 import math
 
-x = np.arange(0,360,45)
-x = x * np.pi / 180
-y = np.sin(x)
+x = np.array([[1,5,3,1],[0,1,2,9]])
 
-print(x)
-print(y)
+print(x); print()
+print(f"np.sum(x): {np.sum(x)}")                    # sum of all axes
+print(f"np.sum(x, axis=0): {np.sum(x, axis=0)}")    # column sum
+print(f"np.sum(x, axis=1): {np.sum(x, axis=1)}")    # row sum
