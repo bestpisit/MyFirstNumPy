adaba7324d9d21d81cea0b490b7c03498591329c
BestEngineer <65943291+bestpisit@users.noreply.github.com>
2024-02-12 22:01:52 +0700

    one zero 1d,2d,3d

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
 import numpy as np
-print(np.__version__)
 
-x1 = np.array([1,2,3,4,5,6])                      # 1D array
-x2 = np.array([[1,2,3],[11,22,33],[10,20,30]])    # 2D array
-
-print('---Print numpy arrays:---')
-print(f"x1 1D-array: \n{x1}")
-print(f"x2 2D-array: \n{x2}")
-
-print('\n------Print 2D list------')
-y =[[1,2,3],[11,22,33],[10,20,30]]
-print(f"y 2D-List: \n{y}")
-
-print(x1.shape,x1.dtype)
-print(x2.shape,x2.dtype)
+a = np.zeros((10,))
+b = np.ones((5,2))
+c = np.zeros((2,5))
+d = np.ones((2,3,4))
+print(a)
+print(b)
+print(c)
+print(d)
