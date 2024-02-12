37a1b0063f80931b72f160b7ab6cc1c32d6001c7
BestEngineer <65943291+bestpisit@users.noreply.github.com>
2024-02-12 22:48:31 +0700

    selector challenge

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 import numpy as np
-import math
 
-# assign values
-x = np.array([
-    [ 5, 6, 7, 8],
-	  [ 9,10,11,12],
-	  [13,14,15,16]])
+x = np.array([[ 1, 10, 1, 3, 5],
+            [ 1, 15, 2, 3, 5],
+            [ 0, 20, 3, 3, 5],
+            [ 1, 25, 4, 3, 5]])
 
-print(x); print()
-x[:,2:] = 0
-print(x)
+y = (x[:,0] == 1)
+
+print(np.sum(x[y][:,[2,4]],axis=1))
